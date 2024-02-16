5321b5df926df236dd1166afb1a719f97f1de661
Jesus <6.jesus.a@gmail.com>
2024-02-15 22:54:01 -0300

    avanzando con ejercicios react

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Button texto={"Good"} variable={good} activador={setGood} />
+      <h1>Entreganos tu feedback</h1>
+      <Button texto={"Good"} tipo={good} activador={setGood} />
+      <Button texto={"Neutral"} tipo={neutral} activador={setNeutral} />
+      <Button texto={"Bad"} tipo={bad} activador={setBad} />
+      <h1>Estidisticas</h1>
+      <Valor texto={"N° de buenos"} valor={good} />
+      <Valor texto={"N° de neutros"} valor={neutral} />
+      <Valor texto={"N° de malos"} valor={bad} />
+      <Total valor1={good} valor2={neutral} valor3={bad}/>
+      <Promedio valor1={good} valor2={neutral} valor3={bad}/>
     </div>
-  )
-}
+  );
+};
 
-const Button = ({texto,variable,activador}) => {
+const Button = ({ texto, tipo, activador }) => {
+  return <button onClick={() => activador(tipo + 1)}>{texto}</button>;
+};
+
+const Valor = ({ texto, valor }) => {
   return (
-    <button onClick={{activador}}>
-      {texto} 
-    </button>
-  )
-}
+    <p>
+      {texto} {valor}
+    </p>
+  );
+};
 
-export default App
+const Total = ({ valor1, valor2, valor3 }) => {
+  const suma = valor1 + valor2 + valor3;
+  return (
+    <>
+      <p> El valor total de los puntos es {suma} </p>
+    </>
+  );
+};
+
+const Promedio = ({ valor1, valor2, valor3 }) => {
+  const suma = (valor1 + valor2 + valor3)/3;
+  return (
+    <>
+      <p> El valor promedio es  {suma} </p>
+    </>
+  );
+};
+
+export default App;
